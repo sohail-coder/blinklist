2066c80e57125c39f3909b2bb86191028251ac3b
sohail-coder <shaiksohail2332000@gmail.com>
2022-06-28 17:48:36 +0530

    site deployed at netlify with modified changes

--- src/components/atoms/Avatar/index.stories.tsx
+++ src/components/atoms/Avatar/index.stories.tsx
@@ -1,14 +1,16 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import CustomAvatar from './index';
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import CustomAvatar from "./index";
 
-export default{
-    title:'atoms/Avatar',
-    component:CustomAvatar
-}as ComponentMeta<typeof CustomAvatar>
+export default {
+  title: "atoms/Avatar",
+  component: CustomAvatar,
+} as ComponentMeta<typeof CustomAvatar>;
 
-const Template:ComponentStory<typeof CustomAvatar> = (args) => <CustomAvatar  {...args}/>;
+const Template: ComponentStory<typeof CustomAvatar> = (args) => (
+  <CustomAvatar {...args} />
+);
 
 export const loginAvatar = Template.bind({});
-loginAvatar.args={
-    children:'A'
-}
+loginAvatar.args = {
+  children: "SK",
+};
